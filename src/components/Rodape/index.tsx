14116cfb876c375f29780b4fb8d3f6719721efd4
Thiago <tpessoa96@gmail.com>
2023-07-27 20:30:41 -0300

    Usando Fetch

--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 import facebook from './assets/facebook.png'
+import whatsapp from './assets/whatsapp.png'
+import instagram from './assets/instagram.png'
+import google from './assets/google.png'
+
 
 const RodapeEstilizado = styled.footer`
     height: 100%
@@ -12,11 +16,14 @@ const RodapeEstilizado = styled.footer`
 const ListaEstilizada = styled.ul`
     display:flex;
     justify-content: space-around;
-    width: 10%;
+    width: 20%;
     margin: 1em auto;
 `
 const ItemEstilizado = styled.li`
     list-style-type: none;
+`
+const LinkEstilizado = styled.a`
+    padding: .5em;
 `
 
 function Rodape(){
@@ -24,12 +31,21 @@ function Rodape(){
         <RodapeEstilizado>
             <ListaEstilizada>
                 <ItemEstilizado>
-                    <a href="#">
+                    <LinkEstilizado href="#">
                         <img src={facebook} alt="logo do facebook" />
-                    </a>
+                    </LinkEstilizado>
+                    <LinkEstilizado href="#">
+                        <img src={instagram} alt="logo do instagram" />
+                    </LinkEstilizado>
+                    <LinkEstilizado href="#">
+                        <img src={whatsapp} alt="logo do whatsapp" />
+                    </LinkEstilizado>
+                    <LinkEstilizado href="#">
+                        <img src={google} alt="logo do google" />
+                    </LinkEstilizado>
                 </ItemEstilizado>
             </ListaEstilizada>
-            <p>2023 Desenvolvido por Alura | Projeto fictício sem fins comerciais.</p>
+            <p style={{color: 'white'}}>2023 Desenvolvido por Alura | Projeto fictício sem fins comerciais.</p>
         </RodapeEstilizado>
     )
 }
