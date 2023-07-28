2fe972b128db74a16ed07ff125c79c6b7613dcb7
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-28 11:57:38 -0300

    verificando consultas

---
+++ src/components/Grafico/index.tsx
@@ -0,0 +1,31 @@
+import { margin } from "@mui/system"
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+import IProfissional from "../../types/IProfissional"
+import IConsulta from "../../types/IConsulta"
+import useDadosGrafico from "./useDadosGraficos"
+
+interface Props{
+    profissionais: IProfissional[] | null,
+    consultas: IConsulta[] | null
+}
+
+interface IDados{
+    nome: string,
+    consultas: number
+}
+
+function Grafico({profissionais, consultas}: Props){
+    let dados:Array<IDados> = useDadosGrafico({profissionais, consultas})
+    console.log(dados)
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <BarChart layout="vertical" data={dados} margin={{top: 25, right: 40, left: 40, bottom: 20}}>
+                <XAxis type="number"></XAxis>
+                <YAxis type="category" dataKey="nome"></YAxis>
+                <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
+            </BarChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default Grafico
