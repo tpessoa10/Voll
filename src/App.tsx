5e97e9b82b287f6c31406ac8005956c93eb46d9c
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-28 11:57:14 -0300

    verificando consultas

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import './App.css';
 import Cabecalho from './components/Cabecalho';
 import Titulo from './components/Titulo';
 import Rodape from './components/Rodape';
+import Tabela from './components/Tabela';
+import useDadosConsulta from './useDadosConsulta';
+import Grafico from './components/Grafico';
+import useDadosProfissionais from './useDadosProfissionais';
+
 
 function App() {
+  const { dados: consultas, erro: consultasErro } = useDadosConsulta()
+  const { dados: profissionais, erro: profissionaisErro } = useDadosProfissionais()
+
+  if(consultasErro || profissionaisErro){
+    console.log("Ocorreu um erro na requisição")
+  }
   return (
     <>
     <Cabecalho/>
     <Container>
       <Titulo>Área administrativa</Titulo>
+      <Tabela consultas={consultas}/>
+      <Grafico consultas={consultas} profissionais={profissionais}/>
     </Container>
     <Rodape/>
     </>
